c78514d44f936435b03e8f290c26ee08bbd8524f
Franz <f.o.larionov@gmail.com>
2024-10-12 15:51:26 +0300

    fix product-form

--- shared/components/shared/choose-product-form.tsx
+++ shared/components/shared/choose-product-form.tsx
@@ -24,22 +24,25 @@ export const ChooseProductForm: React.FC<Props> = ({
   loading,
 }) => {
   return (
-    <div className={cn(className, 'flex flex-1')}>
-      <div className="flex items-center justify-center flex-1 relative w-full">
+    <div className={cn(className, 'flex flex-col md:flex-row flex-1')}>
+      {/* Блок с изображением */}
+      <div className="flex items-center justify-center flex-1 relative w-full mb-4 md:mb-0">
         <img
           src={imageUrl}
           alt={name}
-          className="relative left-2 top-2 transition-all z-10 duration-300 w-[350px] h-[350px]"
+          className="transition-all z-10 duration-300 w-[200px] h-[200px] md:w-[350px] md:h-[350px] object-cover"
         />
       </div>
 
-      <div className="w-[490px] bg-[#f7f6f5] p-7">
-        <Title text={name} size="md" className="font-extrabold mb-1" />
+      {/* Блок с информацией */}
+      <div className="w-full bg-[#f7f6f5] p-4 md:p-7 md:max-w-[490px] mx-auto">
+        <Title text={name} size="md" className="font-extrabold mb-1 text-center md:text-left" />
 
         <Button
           loading={loading}
           onClick={() => onSubmit?.()}
-          className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
+          className="h-[45px] md:h-[55px] px-8 md:px-10 text-base rounded-[18px] w-full mt-4 md:mt-10"
+        >
           Добавить в корзину за {price} ₽
         </Button>
       </div>
